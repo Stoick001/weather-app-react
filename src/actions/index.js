82ed851841fff1efddbe10ae7ffe8ecd30bd973a
Stoick01 <work.gordan.prastalo@gmail.com>
2018-02-11 17:29:57 +0100

    React redux weather app

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = '0ed0a18dfebd9fed89626b393f66a181';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  console.log('Request:', request);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,11 @@
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch(action.type) {
+    case FETCH_WEATHER:
+      return state.concat([action.payload.data]);
+    default:
+      return state;
+  }
+  return state;
+}
